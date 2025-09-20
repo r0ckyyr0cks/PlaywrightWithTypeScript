0c5797658181cf566c1f26fea1207edd30455f8f
rakesh.ranjan.qaengineer@gmail.com <rakesh.ranjan.qaengineer@gmail.com>
2025-09-20 16:35:07 +0530

    working files from Sep 2025

---
+++ tests/Simple.spec.ts
@@ -0,0 +1,4 @@
+//print username and salary
+const name1 = "Neha Rani";
+const salary = 50000;
+console.log(`${name1} has a salary of ${salary} per month.`); // John has a salary of $50000 per year.
