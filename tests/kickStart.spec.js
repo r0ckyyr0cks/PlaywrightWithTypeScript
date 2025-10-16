9ba19822b1684a018bda43e5cff1aa5ac2c65cb6
rakesh.ranjan.qaengineer@gmail.com <rakesh.ranjan.qaengineer@gmail.com>
2025-10-16 17:00:50 +0530

    working files from Oct 2025

---
+++ tests/kickStart.spec.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var test_1 = require("@playwright/test");
+(0, test_1.test)("Kick Start with Playwright", function () {
+    console.log("My First Test Script");
+});
+(0, test_1.test)("Kick Start with 2nd Playwright", function () {
+    console.log("My Second Test Script");
+});
---
+++ tests/kickStart.spec.ts
@@ -0,0 +1,9 @@
+import {test} from '@playwright/test';
+
+test("Kick Start with Playwright", () => {
+    console.log("My First Test Script");
+})
+
+test("Kick Start with 2nd Playwright", () => {
+    console.log("My Second Test Script");
+})
