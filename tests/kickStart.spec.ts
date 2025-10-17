17b926dc368b07e595f970baa65c5f447ef1c023
rakesh.ranjan.qaengineer@gmail.com <rakesh.ranjan.qaengineer@gmail.com>
2025-10-17 21:29:21 +0530

    working files from Oct 2025

--- tests/kickStart.spec.ts
+++ tests/kickStart.spec.ts
@@ -1,9 +1,28 @@
-import {test} from '@playwright/test';
+import {chromium, test} from '@playwright/test';
 
-test("Kick Start with Playwright", () => {
+test("Kick Start with Playwright Test", async ({page}) => {
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+    await page.getByPlaceholder('Email').fill('rahulshetty@gmail.com');
+    await page.getByRole('textbox', { name: 'enter your passsword' }).fill('Iamking@000');
+    await page.getByRole('button', { name: 'Login' }).click();    
     console.log("My First Test Script");
 })
 
-test("Kick Start with 2nd Playwright", () => {
+test("Kick Start with 2nd Playwright Test", async ({page}) => {
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+    await page.getByRole('textbox', { name: 'Email' }).fill('rahulshetty@gmail.com');
+    await page.getByRole('textbox', { name: 'enter your passsword' }).fill('Iamking@000');
+    await page.getByRole('button', { name: 'Login' }).click();    
     console.log("My Second Test Script");
+})
+
+test("Kick Start with 3rd Playwright Test", async () => {
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+    await page.getByRole('textbox', { name: 'Email' }).fill('rahulshetty@gmail.com');
+    await page.getByRole('textbox', { name: 'enter your passsword' }).fill('Iamking@000');
+    await page.getByRole('button', { name: 'Login' }).click();    
+    console.log("My Third Test Script");
 })
